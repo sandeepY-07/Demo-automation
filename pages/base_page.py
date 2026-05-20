0ce7de8c91f4d9686741e715eee7a74693ae9614
Sandeep <sandeepyelisetty111@gmail.com>
2026-05-20 10:35:16 +0530

    updated files

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,5 +1,3 @@
-import time
-
 from selenium.webdriver.support.ui import WebDriverWait
 
 from selenium.webdriver.support import expected_conditions as EC
@@ -11,59 +9,37 @@ class BasePage:
 
         self.driver = driver
 
-
     def click_element(self, locator):
 
-        element = WebDriverWait(
-            self.driver,
-            15
-        ).until(
+        WebDriverWait(self.driver, 10).until(
             EC.element_to_be_clickable(locator)
-        )
-
-        time.sleep(1)
-
-        element.click()
-
-        time.sleep(1)
-
+        ).click()
 
     def enter_text(self, locator, text):
 
         element = WebDriverWait(
             self.driver,
-            15
+            10
         ).until(
             EC.visibility_of_element_located(locator)
         )
 
         element.clear()
 
-        time.sleep(1)
-
         element.send_keys(text)
-
-        time.sleep(1)
-
 
     def get_text(self, locator):
 
-        element = WebDriverWait(
+        return WebDriverWait(
             self.driver,
-            15
+            10
         ).until(
             EC.visibility_of_element_located(locator)
-        )
-
-        return element.text
-
+        ).text
 
     def wait_for_alert_and_accept(self):
 
-        WebDriverWait(
-            self.driver,
-            15
-        ).until(
+        WebDriverWait(self.driver, 10).until(
             EC.alert_is_present()
         )
 
@@ -71,20 +47,10 @@ class BasePage:
 
         text = alert.text
 
-        time.sleep(1)
-
         alert.accept()
-
-        time.sleep(1)
 
         return text
 
-
     def find_element(self, locator):
 
-        return WebDriverWait(
-            self.driver,
-            15
-        ).until(
-            EC.presence_of_element_located(locator)
-        )
+        return self.driver.find_element(*locator)
